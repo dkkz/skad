5e3e6252445780d3ccb3c4fef181b54511631179
Kim <donggui7@gmail.com>
2019-04-21 22:41:31 +0900

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# skad
+Skad is skip youtube Ad for Chrome extensions.
+
+## install
+1. Open Chrome and input URL ``chrome://extensions/``.
+2. On ``Developer mode``.
+3. Click ``Load unpacked``.
+4. Select ``skad`` directory.
---
+++ skip.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function(e){
+    const config = { attributes: true, childList: true, characterData: true, subtree: true };
+    const target = document.querySelector('.video-ads.ytp-ad-module');
+    const observer = new MutationObserver(() => {
+      const adText = document.querySelector('.ytp-ad-text');
+      const skipButton = document.querySelector('.ytp-ad-skip-button');
+      const banner = document.querySelector('.ytp-ad-overlay-image');
+      const closeButton = document.querySelector('.ytp-ad-overlay-close-button');
+      if(adText) skipButton.click();
+      if(banner) closeButton.click();
+    });
+    observer.observe(target, config);
+}, {once: true});

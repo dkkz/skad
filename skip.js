750d50db11aada292e3300de597364cf16e86a20
Kim <donggui7@gmail.com>
2019-04-22 22:57:26 +0900

    Short ad avoid occurring error to click

--- skip.js
+++ skip.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function(e){
     const config = { attributes: true, childList: true, characterData: true, subtree: true };
     const target = document.querySelector('.video-ads.ytp-ad-module');
     const observer = new MutationObserver(() => {
-      const adText = document.querySelector('.ytp-ad-text');
+      const adText = document.querySelector('.ytp-ad-skip-button-text');
       const skipButton = document.querySelector('.ytp-ad-skip-button');
       const banner = document.querySelector('.ytp-ad-overlay-image');
       const closeButton = document.querySelector('.ytp-ad-overlay-close-button');

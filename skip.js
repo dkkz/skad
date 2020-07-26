edc6a3a5ba5806604d61f7fe6874aed31f1ce132
dkthemonkey <donggui7@gmail.com>
2020-07-26 23:19:48 +0900

    change window event fix #1

--- skip.js
+++ skip.js
@@ -1,13 +1,26 @@
-window.addEventListener('load', function(e){
-    const config = { attributes: true, childList: true, characterData: true, subtree: true };
-    const target = document.querySelector('.video-ads.ytp-ad-module');
-    const observer = new MutationObserver(() => {
-      const adText = document.querySelector('.ytp-ad-skip-button-text');
-      const skipButton = document.querySelector('.ytp-ad-skip-button');
-      const banner = document.querySelector('.ytp-ad-overlay-image');
-      const closeButton = document.querySelector('.ytp-ad-overlay-close-button');
-      if(adText) skipButton.click();
-      if(banner) closeButton.click();
-    });
-    observer.observe(target, config);
-}, {once: true});
+window.addEventListener(
+  "click",
+  function() {
+    setTimeout(() => {
+      const config = {
+        attributes: true,
+        childList: true,
+        characterData: true,
+        subtree: true
+      };
+      const target = document.querySelector(".video-ads.ytp-ad-module");
+      const observer = new MutationObserver(() => {
+        const adText = document.querySelector(".ytp-ad-skip-button-text");
+        const skipButton = document.querySelector(".ytp-ad-skip-button");
+        const banner = document.querySelector(".ytp-ad-overlay-image");
+        const closeButton = document.querySelector(
+          ".ytp-ad-overlay-close-button"
+        );
+        if (adText) skipButton.click();
+        if (banner) closeButton.click();
+      });
+      observer.observe(target, config);
+    }, 1000);
+  },
+  { once: true }
+);

dcc748a052090f889dad1369a1af72bdce90aa2d
dkthemonkey <donggui7@gmail.com>
2021-08-12 17:12:01 +0900

    :package: Add compiled file

--- dist/skip.js
+++ dist/skip.js
@@ -1,21 +1,49 @@
-var config = {
-    attributes: true,
-    childList: true,
-    characterData: true,
-    subtree: true,
-};
-export var observeComponent = function (container, button) {
-    var target = document.querySelector(container);
-    var observer = new MutationObserver(function () {
-        var removeButton = document.querySelector(button);
-        removeButton && removeButton.click();
-    });
-    target && observer.observe(target, config);
-};
-['click', 'load'].map(function (event) {
-    return document.addEventListener(event, function () {
-        observeComponent('.button-container.style-scope.ytd-mealbar-promo-renderer', '#dismiss-button #text.style-scope.ytd-button-renderer.style-text.size-default');
-        observeComponent('.video-ads', '.ytp-ad-skip-button');
-        observeComponent('.video-ads', '.ytp-ad-overlay-close-button');
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    exports.__esModule = true;
+    exports.hideElement = exports.observeComponent = exports.skipConfig = exports.observerConfig = void 0;
+    exports.observerConfig = {
+        attributes: true,
+        childList: true,
+        characterData: true,
+        subtree: true
+    };
+    exports.skipConfig = {
+        config: exports.observerConfig,
+        videoContainer: '.ytd-player',
+        adText: '.ytp-ad-skip-button-text',
+        skipButton: '.ytp-ad-skip-button',
+        banner: '.ytp-ad-overlay-image',
+        closeButton: '.ytp-ad-overlay-close-button'
+    };
+    var observeComponent = function (data) {
+        var target = document.querySelector(data.videoContainer);
+        var observer = new MutationObserver(function () {
+            var adText = document.querySelector(data.adText);
+            var skipButton = document.querySelector(data.skipButton);
+            var banner = document.querySelector(data.banner);
+            var closeButton = document.querySelector(data.closeButton);
+            adText && skipButton.click();
+            banner && closeButton.click();
+        });
+        target && observer.observe(target, data.config);
+    };
+    exports.observeComponent = observeComponent;
+    var hideElement = function (element) {
+        var target = document.querySelector(element);
+        return (target.style.display = 'none');
+    };
+    exports.hideElement = hideElement;
+    document.body.addEventListener('yt-navigate-start', function () {
+        exports.observeComponent(exports.skipConfig);
+        exports.hideElement('ytd-popup-container');
     }, { once: true });
 });
